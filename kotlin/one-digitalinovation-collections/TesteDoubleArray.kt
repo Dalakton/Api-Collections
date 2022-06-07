28e98d49f14576e930f15134b89dbbc6593f1174
Dalakton <DalaktonRodriguesoficial@gmail.com>
2022-06-07 16:10:41 -0300

    Resolução do Double array

---
+++ kotlin/one-digitalinovation-collections/TesteDoubleArray.kt
@@ -0,0 +1,19 @@
+package `one-digitalinovation-collections`
+
+fun main() {
+
+    val salarios = DoubleArray(3)
+
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 500.0
+
+    salarios.forEach { println(it) }
+
+    println("-------------------------")
+
+
+    val salarios2 = doubleArrayOf(1500.0, 1250.0, 500.0)
+        salarios2.sort()
+        salarios2.forEach { println(it) }
+}
