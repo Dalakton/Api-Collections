7e37bdbf79ebb77a9a2ae02a386180e8dcb00c64
Dalakton <DalaktonRodriguesoficial@gmail.com>
2022-06-07 13:42:40 -0300

    resolução do arrayOf

---
+++ kotlin/one-digitalinovation-collections/TesteIntArrayOf.kt
@@ -0,0 +1,17 @@
+package `one-digitalinovation-collections`
+
+fun main() {
+
+    val values = intArrayOf(2,3,4,5,6,10)
+
+    values.forEach {
+        println(it)
+    }
+
+    values.sort()
+    values.forEach {
+        println(it)
+    }
+
+
+}
